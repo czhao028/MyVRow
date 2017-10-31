094be9e4b85d35411ee746358677c580bf345bd0
czhao028 <czhao028@gmail.com>
2017-10-31 09:46:48 -0400

    Initial commit

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise{
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                
+            }
+        }
+
+        return noiseMap;
+    }
+}
