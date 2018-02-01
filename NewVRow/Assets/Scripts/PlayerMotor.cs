a55dacfba1dde7efc659bfba78f0db7203de53f1
czhao028 <czhao028@gmail.com>
2018-02-01 10:17:05 -0500

    character prefabs

---
+++ NewVRow/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+    private CharacterController controller;
+    //SUBSTITUTE speed for speed read in from the PM5 monitor
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        controller.Move((Vector3.forward*speed) * Time.deltaTime);
+        Debug.Log(controller.transform.position);
+	}
+}
