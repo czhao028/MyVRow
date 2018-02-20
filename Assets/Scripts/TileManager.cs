962af3858662ef3cfcb2b0ee146521008d3dacad
czhao028 <czhao028@gmail.com>
2018-02-20 10:08:28 -0500

    try size of collider

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -11,14 +11,19 @@ public class TileManager : MonoBehaviour {
     //private float tileLength = 
 
 	void Start () {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").tranform;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         for (int i = 0; i < prefabs.Length; i++)
         {
-            Debug.Log(prefabs[i].collider.bounds.size);
+      
+            foreach(Transform child in prefabs[i].transform)
+            {
+                Debug.Log(child.GetComponent<Collider>().bounds.size);
+            }
+            
         }
 		
 	}
