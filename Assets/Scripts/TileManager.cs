17980ec3a838c95adedc7a57f23bcdcc3916f363
czhao028 <czhao028@gmail.com>
2018-02-22 10:15:50 -0500

    finish infinite tile, in progress - fixing prefabs

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -5,48 +5,48 @@ using UnityEngine;
 public class TileManager : MonoBehaviour {
 
     public GameObject[] prefabs;
+    private int upStairsUpperBound;
     private Transform playerTransform;
     private float spawnZ = -6.0f;
-    //private float tileLength = 
     private int numberTilesScreen = 12;
     private float avgLength = 5.0f;
-
+    private float safeZone = 15.0f;
 
     private Dictionary<GameObject, float> objectZComponents;
-    private List<GameObject> listOfTiles;
 
 	void Start () {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         objectZComponents = new Dictionary<GameObject, float>();
-        listOfTiles = new List<GameObject>();
-
+        upStairsUpperBound = Mathf.FloorToInt((float)prefabs.Length / 2.0f);
+        Debug.Log(upStairsUpperBound);
         for (int i = 0; i < prefabs.Length; i++)
         {
             Vector3 prefabSize = prefabs[i].transform.GetChild(0).GetComponent<Renderer>().bounds.size;
             objectZComponents.Add(prefabs[i], prefabSize.z - 1f);
         }
-
         for (int i = 0; i < numberTilesScreen; i++)
         {
             SpawnTile();
         }
-        
-
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(spawnZ - (avgLength*numberTilesScreen) < playerTransform.position.z)
+        if(safeZone + spawnZ - (avgLength*numberTilesScreen) < playerTransform.position.z)
         {
-            SpawnTile();
+            int rando = Random.Range(0, upStairsUpperBound);
+            SpawnTile(rando);
+            if(rando != 0)
+            {
+                SpawnTile(rando + 1);
+            }
             DeleteChild();
         }
-		
 	}
 
-    private void SpawnTile(int prefabIndex = -1)
+    private void SpawnTile(int prefabIndex = 0)
     {
-        int Counter = 0;
+        int Counter = prefabIndex;
         GameObject go = Instantiate(prefabs[Counter]) as GameObject;
         go.transform.SetParent(this.transform);
         go.transform.position = Vector3.forward * spawnZ;
@@ -57,3 +57,63 @@ public class TileManager : MonoBehaviour {
         Destroy(transform.GetChild(0).gameObject);
     }
 }
+
+//public class TileManager : MonoBehaviour
+//{
+
+//    public GameObject[] prefabs;
+//    private Transform playerTransform;
+//    private float spawnZ = -6.0f;
+//    //private float tileLength = 
+//    private int numberTilesScreen = 12;
+//    private float avgLength = 5.0f;
+
+
+//    private Dictionary<GameObject, float> objectZComponents;
+//    private List<GameObject> listOfTiles;
+
+//    void Start()
+//    {
+//        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+//        objectZComponents = new Dictionary<GameObject, float>();
+//        listOfTiles = new List<GameObject>();
+
+//        for (int i = 0; i < prefabs.Length; i++)
+//        {
+//            Vector3 prefabSize = prefabs[i].transform.GetChild(0).GetComponent<Renderer>().bounds.size;
+//            objectZComponents.Add(prefabs[i], prefabSize.z - 1f);
+//        }
+
+//        for (int i = 0; i < numberTilesScreen; i++)
+//        {
+//            SpawnTile();
+//        }
+
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (spawnZ - (avgLength * numberTilesScreen) < playerTransform.position.z)
+//        {
+//            SpawnTile();
+//            DeleteChild();
+//        }
+
+//    }
+
+//    private void SpawnTile(int prefabIndex = -1)
+//    {
+//        int Counter = 0;
+//        GameObject go = Instantiate(prefabs[Counter]) as GameObject;
+//        go.transform.SetParent(this.transform);
+//        go.transform.position = Vector3.forward * spawnZ;
+//        listOfTiles.Add(go);
+//        spawnZ += objectZComponents[prefabs[Counter]];
+//    }
+//    private void DeleteChild()
+//    {
+//        Destroy(listOfTiles[0]);
+//        listOfTiles.RemoveAt(0);
+//    }
